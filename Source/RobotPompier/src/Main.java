01a8176eb26bc663c66cbf106fc36b873e52747f
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-01-16 09:31:18 +0100

    Ajout du main

---
+++ Source/RobotPompier/src/Main.java
@@ -0,0 +1,15 @@
+import IHM.MainWindow;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) {
+		MainWindow w = new MainWindow();
+		w.setVisible(true);
+	}
+
+}

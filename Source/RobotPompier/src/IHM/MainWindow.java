7dd97f44d7d1b6ab4a8c8637166ac8c073788f9c
Marc Plano-Lesay <marc.planolesay@gmail.com>
2012-01-16 10:22:09 +0100

    Ajout du menu

--- Source/RobotPompier/src/IHM/MainWindow.java
+++ Source/RobotPompier/src/IHM/MainWindow.java
@@ -3,15 +3,24 @@
  */
 package IHM;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class MainWindow extends JFrame {
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+public class MainWindow extends JFrame implements ActionListener {
 	private static final long serialVersionUID = -5464261678769218180L;
 	
 	public MainWindow() {
 		setTitle("Fire Disaster Simulator");
 		simulationView = new SimulationView();
 		add(simulationView);
+		createMenuBar();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
@@ -83,5 +92,23 @@ public class MainWindow extends JFrame {
 		// begin-user-code
 		this.robotModelView2 = robotModelView2;
 		// end-user-code
+	}
+	
+	private void createMenuBar() {
+		JMenuBar bar = new JMenuBar();
+		JMenu simulation = new JMenu("Simulation");
+		JMenuItem quit = new JMenuItem("Quitter");
+		quit.setMnemonic('q');
+		quit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+		quit.getAccessibleContext().setAccessibleDescription("Quitte l'application");
+		quit.addActionListener(this);
+		simulation.add(quit);
+		bar.add(simulation);
+		setJMenuBar(bar);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.exit(0);
 	}
 }

bc0a10e17576c304143d2bc0676554487299cd56
Adrien Vetillart <adrien.vetillart@gmail.com>
2012-01-22 18:15:40 +0100

    Ajout de robot model et test OK du scrollPane

--- Source/RobotPompier/src/IHM/SimulationView.java
+++ Source/RobotPompier/src/IHM/SimulationView.java
@@ -3,13 +3,25 @@
  */
 package IHM;
 
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class SimulationView extends JPanel {
 	private static final long serialVersionUID = 1L;
+	private JPanel timerPanel;
+	private JLabel timer;
 	public SimulationView() {
-		add(new JLabel("Statistiques"));
+		this.setSize(50, 100);
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS ));
+		this.add(new JLabel("Statistiques"));
+		showTimer();
+	}
+	
+	public void showTimer()
+	{
+		timerPanel = new JPanel();
+		this.add(timerPanel);
 	}
 }

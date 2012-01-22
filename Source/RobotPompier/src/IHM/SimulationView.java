ff489327ef0c87c0f7621457c5c589b89366e3f8
Adrien Vetillart <adrien.vetillart@gmail.com>
2012-01-22 19:15:56 +0100

    Ajout de la classe Timer

--- Source/RobotPompier/src/IHM/SimulationView.java
+++ Source/RobotPompier/src/IHM/SimulationView.java
@@ -3,25 +3,20 @@
  */
 package IHM;
 
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class SimulationView extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JPanel timerPanel;
-	private JLabel timer;
 	public SimulationView() {
-		this.setSize(50, 100);
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS ));
+		//this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS ));
 		this.add(new JLabel("Statistiques"));
-		showTimer();
+		//showTimer();
 	}
 	
 	public void showTimer()
 	{
-		timerPanel = new JPanel();
-		this.add(timerPanel);
+		//TODO
 	}
 }

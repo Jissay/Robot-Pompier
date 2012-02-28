82265b50d67347088f1b70f201e3d0895dad796f
Buffon Nicolas <jokahero@gmail.com>
2012-02-28 15:17:04 +0100

    Mise en place du look and feel de l'application en fonction du système sur lequel on le lance.

--- Source/RobotPompier/src/Main.java
+++ Source/RobotPompier/src/Main.java
@@ -8,9 +8,23 @@ import Model.Map;
 import Model.Robot;
 import Model.algorithms.Algorithm;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
 		MapController ctrl = new MapController();
 		Map map = new Map(10, 10);
 		MainWindow w = new MainWindow();

d2b9b89e5a7a2a0aae1bd43a568cca7cf361088b
Nicolas Buffon <akara@pixelized.fr>
2012-05-03 14:22:58 +0200

    Modification de la classe SimulationThread, elle hérite maintenant de TimerTask.

--- Source/RobotPompier/src/Model/SimulationThread.java
+++ Source/RobotPompier/src/Model/SimulationThread.java
@@ -1,32 +1,25 @@
 package Model;
 
 import java.util.HashSet;
+import java.util.TimerTask;
 
-public class SimulationThread extends Thread {
-	private boolean _running = true;
+public class SimulationThread extends TimerTask {
 	private Simulation _context;
-	
+
 	public SimulationThread(Simulation context) {
 		_context = context;
 	}
-	
+
 	@Override
 	public void run() {
-		while (_running) {
-			// Chaque robot vérifie si la case qu'il doit éteindre est encore en feu ou non
-			HashSet<Robot> robots = (HashSet<Robot>) _context.getRobotList();
-			for (Robot r : robots)
-				r.checkDestinationStillOnFire();
-			
-			for (Robot r : robots)
-				r.work();
-			
-			_context.getManager().checkForFire();
-		}
-	}
-	
-	public void halt() {
-		_running = false;
-		interrupt();
+		// Chaque robot vérifie si la case qu'il doit éteindre est encore en feu ou non
+		HashSet<Robot> robots = (HashSet<Robot>) _context.getRobotList();
+		for (Robot r : robots)
+			r.checkDestinationStillOnFire();
+
+		for (Robot r : robots)
+			r.work();
+
+		_context.getManager().checkForFire();
 	}
 }
